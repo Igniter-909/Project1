029e14e77959ff9c7c47b634cf2ea47bed88c386
Igniter-909 <igniterofficial909505@gmail.com>
2024-09-22 23:41:00 +0530

    'setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)-> List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requiremnets = [req.replace("\n","") for req in requirements]
+        if HYPEN_E_DOT in requiremnets:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+
+setup(
+    name = 'Project1',
+    version = '0.0.1',
+    author = 'Roshan',
+    author_email = 'igniterofficial909505@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+
+)
